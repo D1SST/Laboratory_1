913c3325e2fe0638d342b41fc27cf4ca389208d9
Данила Александров <49730037+D1SST@users.noreply.github.com>
2023-02-05 16:40:37 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+from num2words import num2words
+
+sequence = []
+f = open("random.txt", "r")
+main_sequence = [int(i) for i in f.read().split(",")]
+for i in range(len(main_sequence)):
+    if (main_sequence[i] != 0) and (main_sequence[i] % 2 == 0) and (main_sequence[i] <= len(main_sequence)):
+        if (i % 2 != 0):
+            print(num2words(main_sequence[i], lang = "ru"), "| иднекс числа -", i)
+
+        else:
+            print(main_sequence[i], "| индекс числа -", i)
+        i += 1
+
+
+
+
+
+
+
+
+
+
